1e78f9d7050260adff93feb5660c11f89a234eb7
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-09 16:05:38 -0600

    Testing vendors 8

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,7 +2,7 @@ Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
-  lookup: :google,
+  lookup: :google_premier,
   ip_lookup: :freegeoip,
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
