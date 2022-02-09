247a2ca913d1f51ebdb11aea922d7999bfa36dc4
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-09 15:13:17 -0600

    Testing vendors 3

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,12 +3,14 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
   lookup: :google,
+  ip_lookup: :freegeoip,
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
-  use_https: true,           # use HTTPS for lookup requests? (if supported)
+  use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV['GOOGLE_API_KEY'], # API key for geocoding service
+  # api_key: ENV['GOOGLE_API_KEY'], # API key for geocoding service
+  api_key: nil,
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # DEPRECATED, please use cache_options[:prefix] instead
 
