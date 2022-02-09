fa4d07ff764ee767558e89bb2518f697411ac49d
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-09 14:08:41 -0600

    Testing looking for ip_address location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @latitude = request.location.latitude
     @longitude = request.location.longitude
     @coordinates = request.location.coordinates
+    @ip_address = request.location.ip_address
+    @ip_coordinates = Geocoder.search(@ip_address).coordinates
   end
 end
