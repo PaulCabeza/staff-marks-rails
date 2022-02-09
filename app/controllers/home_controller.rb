9bbd5749a0b821182e386e07762dcbc8ec8165d3
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-09 13:56:58 -0600

    Testing coordinates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @latitude = request.location.latitude
     @longitude = request.location.longitude
+    @coordinates = request.location.coordinates
   end
 end
