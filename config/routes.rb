bc1408fe18b1f61da49e133513c4b1007acb183e
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-03-21 12:50:32 -0600

    added admin menu and started development of main admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
   devise_for :users
   resources :marks, only: [:index, :new, :create, :show, :destroy]
 
-  get 'users', to: "home#index"
+  get 'users', to: "users#index"
   root to: "marks#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
