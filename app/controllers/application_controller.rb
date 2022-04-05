b9db246d23e42044d00e0ab37205b1735d25c7fd
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-04-05 13:59:12 -0600

    added custom main page for both users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 	include Pagy::Backend
+
+	def after_sign_in_path_for(user)
+        if user.staff?
+        	users_path
+        else
+        	marks_path
+        end        
+    end
+
 end
