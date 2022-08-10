2a10f571abadbc80e36c6f301dc17500e88ed0f2
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-08-10 16:40:11 -0600

    Added employee name on navbar with a link to edit profile

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,13 +1,3 @@
 class Mark < ApplicationRecord
 	belongs_to :user
-
-	# private
-	# ransacker :purchaseorder do
-	# 	table_name = 'marks'
-	# 	Arel.sql("to_char(\"#{table_name}\".\"purchaseorder\", '99999')")
-	# 	# Arel.sql("CONVERT(marks.purchaseorder, CHAR(8))")
-		
-	# end
-
-	
 end
